85d53641d1c4e343315ed022b362713c8eabcad9
Chloe Smith <chlsmi040904@gmail.com>
2024-07-06 17:50:03 +0200

    made home page

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
+  {
+    path: '/resume',
+    name: 'resume',
+    component: ()=>import('@/views/ResumeView.vue')
+  },
 ]
 
 const router = createRouter({
